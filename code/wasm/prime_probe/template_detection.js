c3a143e76ca3c58d4461a9e0a244b33d69e829f1
Moritz Krebbel <moritz.krebbel@student.uni-luebeck.de>
2018-08-14 18:58:44 +0200

    update latex

--- code/wasm/prime_probe/template_detection.js
+++ code/wasm/prime_probe/template_detection.js
@@ -1,22 +1,2 @@
-var numberOfSamples = 300;
-Module['asm']._find_interesting_eviction_sets(Module['appStatePtr'], 1.5, numberOfSamples);
-
-var numberOfObservedCacheSets = Module['numberOfObservedCacheSets'];
-console.log(numberOfObservedCacheSets);
-var wasmMemoryArray = new Uint32Array(Module['wasmMemory'].buffer);
-var idleArrOffset = Module['idleTimesPtr']/Module['timesArrayByteFactor'];
-var currentArrOffset = Module['currentTimesPtr']/Module['timesArrayByteFactor'];
-var interestingCacheSets = "";
-var interestingCounter = 0;
-for(var i=0; i<numberOfObservedCacheSets; i++){
-    //console.log(wasmMemoryArray[idleArrOffset] + " " + wasmMemoryArray[current_arr_offset] + " ");
-    if(wasmMemoryArray[idleArrOffset++] * 1.7 < wasmMemoryArray[currentArrOffset++]){
-        interestingCacheSets += i + " ";
-        interestingCounter++;
-    }
-}
-if(interestingCacheSets == ""){
-    interestingCacheSets = "nothing found!";
-}
-console.log(interestingCacheSets);
-console.log("found "+ interestingCounter);
+//run gcd op in while 
+//process interessting cache sets for shift op first
