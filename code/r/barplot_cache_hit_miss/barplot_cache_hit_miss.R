3de84a34e03df369210a28d69f5a8fcd26fefecd
Moritz Krebbel <moritz.krebbel@student.uni-luebeck.de>
2018-11-03 20:18:40 +0100

    update latex_post_final

--- code/r/barplot_cache_hit_miss/barplot_cache_hit_miss.R
+++ code/r/barplot_cache_hit_miss/barplot_cache_hit_miss.R
@@ -1,4 +1,4 @@
-setwd("~")
+setwd("~/MA_2/master-moritz_krebbel-drive_by_cache_attacks/code/r/barplot_cache_hit_miss/")
 filename <- "miss.txt"
 miss <- (sapply(strsplit(readChar(filename, file.info(filename)$size), " "), as.numeric))
 filename <- "hit.txt"
@@ -18,10 +18,10 @@ ggplot2TikzPlot <- function(name, ggplot, width = 6, height = 4){
 }
 
 
-plot <- ggplot2::ggplot(df_melt, aes(value)) + ggplot2::geom_density() +
+plot <- ggplot2::ggplot(df_melt, ggplot2::aes(value)) + ggplot2::geom_bar(ggplot2::aes(y = (..count..)/sum(..count..))) +
 ggplot2::scale_x_continuous(name="Zugriffszeit in Taktzyklen", 
-                            limits=c(0, 350), 
-                            breaks=c(seq(0,350,50)))+
+                            limits=c(50, 260), 
+                            breaks=c(seq(50,300,50)))+
 ggplot2::scale_y_continuous(name="Wahrscheinlichkeit") 
 
 ggplot2TikzPlot("cache_hit_miss_density.tex", plot)
