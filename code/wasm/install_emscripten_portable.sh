650bdf6a9c07273d6d4612da1f70405e43cf1cda
Moritz Krebbel <moritz.krebbel@student.uni-luebeck.de>
2019-05-25 21:55:05 +0200

    update emscripten install script

--- code/wasm/install_emscripten_portable.sh
+++ code/wasm/install_emscripten_portable.sh
@@ -1,9 +1,15 @@
-wget -O emsdk-portable.tar.gz https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
-tar -xvzf emsdk-portable.tar.gz
-rm emsdk-portable.tar.gz
-cd emsdk-portable
-./emsdk update
+# https://emscripten.org/docs/getting_started/downloads.html
+# Get the emsdk repo
+git clone https://github.com/emscripten-core/emsdk.git
+
+# Enter that directory
+cd emsdk
+
+# Download and install the latest SDK tools.
 ./emsdk install sdk-1.38.11-64bit
+
+# Make the "latest" SDK "active" for the current user. (writes ~/.emscripten file)
 ./emsdk activate sdk-1.38.11-64bit
-#at each startup
+
+# Activate PATH and other environment variables in the current terminal
 source ./emsdk_env.sh
