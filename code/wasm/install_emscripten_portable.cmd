e2b668409d63c2b4aac2eb72fbff5769ea3c562c
Moritz Krebbel <moritz.krebbel@student.uni-luebeck.de>
2019-05-25 22:18:11 +0200

    update install scripts

--- code/wasm/install_emscripten_portable.cmd
+++ code/wasm/install_emscripten_portable.cmd
@@ -2,8 +2,8 @@ wget -O emsdk-portable.tar.gz https://s3.amazonaws.com/mozilla-games/emscripten/
 tar -xvzf emsdk-portable.tar.gz
 rm emsdk-portable.tar.gz
 cd emsdk-portable
-./emsdk update
-./emsdk install sdk-1.38.11-64bit
-./emsdk activate sdk-1.38.11-64bit
+call emsdk update
+call emsdk install sdk-1.38.11-64bit
+call emsdk activate sdk-1.38.11-64bit
 #at each startup
-source ./emsdk_env.sh
+emsdk_env.bat
