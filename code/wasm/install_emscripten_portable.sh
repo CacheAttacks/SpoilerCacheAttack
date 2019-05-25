f784d02f7c6b4faf4398daf38dfc30f950bb784a
Moritz Krebbel <moritz.krebbel@automite.de>
2019-05-25 21:43:03 +0200

    fix emscripten to sdk-1.38.11-64bit

--- code/wasm/install_emscripten_portable.sh
+++ code/wasm/install_emscripten_portable.sh
@@ -3,7 +3,7 @@ tar -xvzf emsdk-portable.tar.gz
 rm emsdk-portable.tar.gz
 cd emsdk-portable
 ./emsdk update
-./emsdk install latest
-./emsdk activate latest
+./emsdk install sdk-1.38.11-64bit
+./emsdk activate sdk-1.38.11-64bit
 #at each startup
 source ./emsdk_env.sh
